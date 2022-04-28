9973740f52a87b6b0ecd5554cc76b853093898f1
Srdjan Margetic <srdjan1983@live.com>
2022-04-28 12:13:18 -0400

    Create root for the create users and change varible in the error_messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
                        length: { minimum: 3, maximum: 25 }
                        
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :email, presence: true, 
                     uniqueness: { case_sensitive: false }, 
                     length: { maximum: 105 },
