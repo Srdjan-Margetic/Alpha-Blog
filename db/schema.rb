9a5fe96257f219982ddae39e77f6d9a0ae443ecf
Srdjan Margetic <srdjan1983@live.com>
2022-05-01 23:45:41 -0400

    Create admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_050838) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_034042) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_050838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin"
   end
 
 end
